d6f7860c6d53f1e0958743f360d65ce8a7a1554f
codingcat <istommao@gmail.com>
2021-05-17 14:02:41 +0800

    Modify otp.py

--- cryptokit/otp.py
+++ cryptokit/otp.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 import os
+import time
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.twofactor.hotp import HOTP
@@ -16,6 +17,28 @@ def generate_htop(length=6, salt=0):
     hotp = HOTP(key, length, SHA1(), backend=default_backend())
     hotp_value = hotp.generate(salt)
 
-    #  hotp.verify(hotp_value, 0)
-
     return hotp_value.decode()
+
+
+def is_valid_totp(totp_value: str):
+    key = os.urandom(20)
+    totp = TOTP(key, 6, SHA1(), 30)
+
+    time_value = time.time()
+
+    try:
+        totp.verify(totp_value.encode('utf-8'), time_value)
+    except:
+        return False
+    else:
+        return True
+
+
+def get_provisioning_uri(key: str):
+    totp = TOTP(key.encode('utf-8'), 6, SHA1(), 30)
+
+    issuer_name = 'crypokit Inc'
+    account_name = 'crypokit@email.com'
+
+    totp_uri = totp.get_provisioning_uri(account_name, issuer_name)
+    return totp_uri
